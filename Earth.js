463cdd3268c5ed4ecb965e3de0cd3dad725b8a84
PritSavaliya1 <117623350+PritSavaliya1@users.noreply.github.com>
2022-11-07 12:08:01 +0530

    Add files via upload

---
+++ Earth.js
@@ -0,0 +1,11 @@
+function showMessage(){
+    alert("Your Login Detail Has Been Saved Click Ok For Continue !");
+}
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
